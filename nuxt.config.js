ca459f0706006bafcb03ae7d51dc93c8b47a490a
John <John.feldhoffer@paf.com>
2020-10-19 16:06:03 +0300

    added asyncdatatest

---
+++ nuxt.config.js
@@ -0,0 +1,13 @@
+export default {
+    server: {
+        port: 4000, // default: 3000
+    },
+        // other configs
+    modules: [
+        '@nuxt/http',
+    ],
+
+    http: {
+        // proxyHeaders: false
+    },
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-    <h1>Hello world!</h1>
+  <div><h1>The sun went up in Frebbenby at:</h1>
+
+    <pre>{{post.location.time[0].sunrise.time}}</pre>
+  </div>
 </template>
+
+<script>
+export default {
+  async asyncData({params, $http}) {
+    const post = await $http.$get('https://api.met.no/weatherapi/sunrise/2.0/.json?lat=60.218718&lon=-19.679863&date=2020-10-19&offset=-00:00')
+    return {post}
+  }
+}
+</script>
+
